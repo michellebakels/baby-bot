652ac4b14c12f58e33c076a84b04f9a732843c3b
michellebakels <michelle.bakels@gmail.com>
2024-05-07 14:18:39 -0400

    add mozilla command

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -1,15 +1,20 @@
 import os
 import logging
+import pandas as pd
+import numpy as np
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 from openai import OpenAI
+from questions import answer_question
 
 load_dotenv()  # take environment variables from .env.
 
 openai = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 
+df = pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 messages = [{
   "role": "system",
@@ -34,13 +39,19 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=completion_answer.content)
 
+async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      answer = answer_question(df, question=update.message.text, debug=True)
+      await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
 if __name__ == '__main__':
   application = ApplicationBuilder().token(tg_bot_token).build()
 
   start_handler = CommandHandler('start', start)
   chat_handler = CommandHandler('chat', chat)
+  mozilla_handler = CommandHandler('mozilla', mozilla)
   
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
+  application.add_handler(mozilla_handler)
 
   application.run_polling()
